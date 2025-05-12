67f300f2092ebe74546082f1c2f56cc72f4af2b9
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2025-05-12 16:12:04 +0600

    Create a blank window

--- core/src/main/java/com/nasim/Main.java
+++ core/src/main/java/com/nasim/Main.java
@@ -1,34 +1,37 @@
 package com.nasim;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f,1f);
-        batch.begin();
-        batch.draw(image, 160, 230);
-        batch.end();
+
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        spriteBatch.dispose();
+
     }
 }
