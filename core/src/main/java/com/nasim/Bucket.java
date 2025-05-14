570c2f6f4f882d8e1304df0fc3f14480629aba22
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2025-05-14 21:56:59 +0600

    Create a bucket class

---
+++ core/src/main/java/com/nasim/Bucket.java
@@ -0,0 +1,33 @@
+package com.nasim;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Bucket {
+    private final Sprite sprite;
+
+    public Bucket() {
+        sprite = new Sprite(AssetLoader.bucket);
+        sprite.setSize(1, 1);
+        sprite.setPosition(3.5f, 0); // Center-ish starting position
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+
+
+    public void moveX(float amount) {
+        sprite.translateX(amount);
+    }
+
+    public void setCenterX(float x) {
+        sprite.setCenter(x, 0.5f);
+    }
+
+    public void clampPosition(float worldWidth) {
+        float maxX = worldWidth - sprite.getWidth();
+        if (sprite.getX() < 0) sprite.setX(0);
+        if (sprite.getX() > maxX) sprite.setX(maxX);
+    }
+}
