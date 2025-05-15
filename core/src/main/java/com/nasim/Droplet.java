1b747bcae6a028d3a2643e953ab9353898c61e7f
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2025-05-15 23:34:51 +0600

    Create Droplet class

---
+++ core/src/main/java/com/nasim/Droplet.java
@@ -0,0 +1,33 @@
+package com.nasim;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Droplet {
+    private final Sprite sprite;
+    private final Rectangle bounds;
+
+    public Droplet(float x, float y) {
+        sprite = new Sprite(AssetLoader.droplet);
+        sprite.setSize(1, 1);
+        sprite.setPosition(x, y);
+        bounds = new Rectangle();
+    }
+
+    public void update(float delta) {
+        sprite.translateY(-2f * delta);
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public Rectangle getBounds() {
+        bounds.set(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
+        return bounds;
+    }
+
+    public boolean isOffScreen() {
+        return sprite.getY() + sprite.getHeight() < 0;
+    }
+}
