8f223d0b433b803d902c85a4248a348303264222
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2025-05-17 23:42:17 +0600

    Linked all classes with Main class

--- core/src/main/java/com/nasim/Bucket.java
+++ core/src/main/java/com/nasim/Bucket.java
@@ -1,20 +1,27 @@
 package com.nasim;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Bucket {
     private final Sprite sprite;
+    private final Rectangle bounds;
 
     public Bucket() {
         sprite = new Sprite(AssetLoader.bucket);
         sprite.setSize(1, 1);
         sprite.setPosition(3.5f, 0); // Center-ish starting position
+        bounds = new Rectangle();
     }
 
     public Sprite getSprite() {
         return sprite;
     }
 
+    public Rectangle getBounds() {
+        bounds.set(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
+        return bounds;
+    }
 
     public void moveX(float amount) {
         sprite.translateX(amount);
--- core/src/main/java/com/nasim/GameWorld.java
+++ core/src/main/java/com/nasim/GameWorld.java
@@ -10,14 +10,37 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 public class GameWorld {
     private final FitViewport viewport;
     private final Bucket bucket;
+    private final Array<Droplet> droplets;
+    private float dropTimer = 0f;
 
     public GameWorld(FitViewport viewport, Bucket bucket) {
         this.viewport = viewport;
         this.bucket = bucket;
+        this.droplets = new Array<>();
     }
 
     public void update() {
+        float delta = com.badlogic.gdx.Gdx.graphics.getDeltaTime();
 
+        bucket.clampPosition(viewport.getWorldWidth());
+
+        for (int i = droplets.size - 1; i >= 0; i--) {
+            Droplet drop = droplets.get(i);
+            drop.update(delta);
+
+            if (drop.isOffScreen()) {
+                droplets.removeIndex(i);
+            } else if (drop.getBounds().overlaps(bucket.getBounds())) {
+                droplets.removeIndex(i);
+                AssetLoader.dropSound.play();
+            }
+        }
+
+        dropTimer += delta;
+        if (dropTimer > 1f) {
+            dropTimer = 0;
+            spawnDroplet();
+        }
     }
 
     public void draw(SpriteBatch batch) {
@@ -26,8 +49,19 @@ public class GameWorld {
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
         batch.draw(AssetLoader.background, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
+        bucket.getSprite().draw(batch);
 
+        for (Droplet drop : droplets) {
+            drop.getSprite().draw(batch);
+        }
 
         batch.end();
+    }
+
+    private void spawnDroplet() {
+        float worldWidth = viewport.getWorldWidth();
+        float x = MathUtils.random(0f, worldWidth - 1);
+        float y = viewport.getWorldHeight();
+        droplets.add(new Droplet(x, y));
     }
 }
